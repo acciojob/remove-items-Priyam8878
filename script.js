8e7456554bd83463b08e2de515ec2ee78e53eabe
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-25 17:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// const color = document.getElementBYId("colorSelect");
+// const btn = document.getElementBYTag("input");
+// btn.addEventListner(click, () =>{
+// 	remove.color
+// });
+<script>
+  document.querySelector('input[type="button"]').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+    colorSelect.remove(selectedIndex);
+  });
+</script>
